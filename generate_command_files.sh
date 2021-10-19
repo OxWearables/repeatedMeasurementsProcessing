b872c9177d7cdfc55f42b591fe0494857d16ef28
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2021-10-19 09:32:56 +0100

    improve file generation script

--- generate_command_files.sh
+++ generate_command_files.sh
@@ -2,11 +2,8 @@
 python write_scripts.py
 
 # Retrofit how to call correct epoch folder
-for i in {0..4}
-do
-    sed -i 's/oct21_willetts_full\/epoch/oct21_walmsley\/epoch/g' processCmds/willetts_full/*
-    sed -i 's/oct21_willetts_reduced\/epoch/oct21_walmsley\/epoch/g' processCmds/willetts_reduced/*
-done
+sed -i 's/oct21_willetts_full\/epoch/oct21_walmsley\/epoch/g' processCmds/willetts_full/*
+sed -i 's/oct21_willetts_reduced\/epoch/oct21_walmsley\/epoch/g' processCmds/willetts_reduced/*
 
 # Retrofit new syntax for calling accProcess from accelerometer
-sed -i 's/accProcess.py/accProcess/g' processCmds/*/*
+sed -i 's/python3 accProcess.py/accProcess/g' processCmds/*/*
